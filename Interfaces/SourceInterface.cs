014e23e434eca3e32b3f62dd94b157daa6d010c5
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-01-31 14:17:37 +1100

    Add `SourceInterface`.

---
+++ Interfaces/SourceInterface.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Aperture.Interfaces;
+
+/// <summary>
+/// A Source-Engine C++ interface.
+/// </summary>
+public class SourceInterface
+{
+    public string InterfaceName { get; }
+    public string ModuleName { get; }
+
+    // The handle to the C++ instance
+    private IntPtr handle;
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    delegate IntPtr CreateInterfaceDelegate(string name, int returnCode);
+
+    /// <summary> Initializes a new instance of the <see cref="SourceInterface"/> class. </summary>
+    public SourceInterface(string interfaceName, string moduleName)
+    {
+        InterfaceName = interfaceName;
+        ModuleName = moduleName;
+
+        CreateInstance();
+    }
+    
+    private void CreateInstance()
+    {
+        IntPtr lib = NativeLibrary.Load(ModuleName);
+        IntPtr func = NativeLibrary.GetExport(lib, "CreateInterface");
+
+        var createInterface = Marshal.GetDelegateForFunctionPointer<CreateInterfaceDelegate>(func);
+        handle = createInterface(InterfaceName, 0);
+    }
+}
