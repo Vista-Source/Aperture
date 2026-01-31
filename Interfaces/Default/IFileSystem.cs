a84615717d07ddb64272f7ecf3b375cb15d43622
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2026-02-01 09:57:21 +1100

    Add default interface system.

---
+++ Interfaces/Default/IFileSystem.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace Aperture.Interfaces.Default;
+
+public class IFileSystem : SourceInterface
+{
+    [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+    delegate bool IsSteamDelegate(IntPtr thisPtr);
+
+    /// <inheritdoc/>
+    public IFileSystem(string interfaceName) : base(interfaceName) { }
+
+    public bool IsSteam() => VTable.GetFunction<IsSteamDelegate>(22)(Handle);
+}
